e2ec9e0a0c00021836ba0d6a32211fede3bdb1b0
Szymon Czarny <sz.p.czarny@gmail.com>
2019-09-23 16:53:36 +0200

    Futher merge adjustments

--- BachelorProject/Source/scene/Window.cpp
+++ BachelorProject/Source/scene/Window.cpp
@@ -1,4 +1,5 @@
 #include "Window.h"
+#include "Window.h"
 
 Window::Window(int width, int height, std::string name)
 	: width { width }
@@ -37,6 +38,15 @@ bool Window::init()
 	glfwSetScrollCallback(glfwWindow, GlfwWindowMouseScrollCallback);
 
 	glfwSetInputMode(glfwWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	glewExperimental = GL_TRUE;
+
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	glDebugMessageCallback(MessageCallback, 0);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+	glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_MARKER, 0,
+		GL_DEBUG_SEVERITY_NOTIFICATION, -1, "Started debugging");
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -101,6 +111,13 @@ void Window::GlfwWindowMouseScrollCallback(GLFWwindow* window, double xoffset, d
 	actualWindow->handleMouseMove(xoffset, yoffset);
 }
 
+void GLAPIENTRY Window::GlfwWindowMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+	fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+		(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+		type, severity, message);
+}
+
 void Window::GlfwWindowResizeCallback(GLFWwindow* window, int width, int height)
 {
 	Window* actualWindow = (Window*)glfwGetWindowUserPointer(window);
@@ -148,5 +165,7 @@ void Window::handleMouseScroll(double xoffset, double yoffset)
 			listener->handleMouseScroll(yoffset);
 		});
 }
+
+
 
 
--- BachelorProject/Source/scene/Window.h
+++ BachelorProject/Source/scene/Window.h
@@ -29,10 +29,11 @@ public:
 
 	int getWidth() const { return width; }
 	int getHeight() const { return height; }
+
+	GLFWwindow* glfwWindow;
 protected:
 private:
-	GLFWwindow* glfwWindow;
-
+	
 	int width;
 	int height;
 	std::string name;
